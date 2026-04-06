db7eefce44d390b7c3a48ef39380aeecd7afb7cf
GLAXT <theglaxt@gmail.com>
2026-04-06 17:54:13 +0100

    fds mode

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿function charger(templateId){
+  const template = document.getElementById(templateId);
+  const mainDisplay = document.getElementById('main-display');
+  const pageTitle = document.getElementById('page-title');
+  if (!template) {
+    console.log("erro")
+  };
+  const nouveauTitre = template.getAttribute('data-titre');
+  const nouveauContenu = template.content.cloneNode(true);
+  pageTitle.innerText = nouveauTitre;
+  mainDisplay.innerHTML ="";
+  mainDisplay.appendChild(nouveauContenu);
+}
+window.onload = () => { 
+  charger('page-accueil');
+};
