a1ae5718c9c3799d2803dd0bc0dce3caeb172cda
GLAXT <theglaxt@gmail.com>
2026-05-05 16:35:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ window.onload = () => {
 const btn = document.getElementById('bouton');
 const pageBody = document.body;
 btn.addEventListener('click', () => {
-  alert('Cette fonctionnalité nécessite une bonne vitesse de connexion internet')
   pageBody.classList.toggle('modeFont');
   btn.textContent = "!"
 });
