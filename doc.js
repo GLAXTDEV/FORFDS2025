2d0e2f16dfe42a0d5b36858c491e9c6096a8963a
GLAXT <theglaxt@gmail.com>
2026-07-28 13:36:13 +0000

    Update doc.js

--- doc.js
+++ doc.js
@@ -288,9 +288,9 @@ const btne = document.querySelector('#bouton');
 
 btne.addEventListener('click', () => {
   const avert = window.confirm("Attention : L'accès à cette page nécessite une bonne connexion internet. L'utilisation d'une connexion limitée est déconseillée. Voulez-vous continuer ?");
-  if (avert) {
+  if (avert === true) {
     window.location.href = "https://glaxtdev.github.io/VIDEO-PAGE/";
-  }
+  } else{ window.location.href = ""}
 });
 
 
